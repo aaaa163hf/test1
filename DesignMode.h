7ed740a4fa0437ba17446228db9f61eb54494091
xingsen.lu <595096620@qq.com>
2021-05-12 19:10:38 +0800

    完成23种设计模式

--- DesignMode.h
+++ DesignMode.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <unordered_map>
 #include <vector>
+#include <stack>
 #include <random>
 using namespace std;
 //*************************创建者模式******************************************************************************//
@@ -182,15 +183,16 @@ singleton* m_pInstance(nullptr);
 /*与深拷贝的区别：由于多态的原因，可以通过父指针克隆一个实例，而深拷贝不行*/
 class base {
 public:
-	virtual std::shared_ptr<base> Clone() = 0;
+	virtual std::shared_ptr<base> clone() = 0;
 };
 class object :public base {
 public:
 	object(int a, int *b): A(a),B(b){}
-	virtual std::shared_ptr<base> Clone() override{ 
+	virtual std::shared_ptr<base> clone(){ 
 		int *b = new int;
 		*b = *B;
-		return std::make_shared<object>(A, b);
+		std::shared_ptr<base> p(new object(A, b));
+		return p;
 	}
 private:
 	int A;
@@ -501,10 +503,404 @@ public:
 	}
 };
 //*************************行为型模式******************************************************************************//
-//44444444444444444444444444444**********命令模式**********444444444444444444444444444444444//
+//44444444444444444444444444444**********解释器模式**********4444444444444444444444444444444444444444444444444//
 //******************************************************************************//
 
+/*(a+(b+c))的调用形式为AndTerminalExpress(terminalExpress,AndTerminalExpress(terminalExpress,terminalExpress))*/
+class Context {
+private:
+	std::unordered_map<terminalExpress*, int> m_mapTeminalValue;
+public:
+	int lookUp(terminalExpress* key) {
+		try {
+			auto it = m_mapTeminalValue.find(key);
+			if (it != m_mapTeminalValue.end()) {
+				return it->second;
+			}
+			throw("don't find the value.");
+		}
+		catch (const char* str) {
+			printf("%s", str);
+		}
+		return 0;
+	}
+};
+class express {
+public:
+	virtual int interpret(Context context) = 0;
+};
+//终端命令
+class terminalExpress :public express {
+public:
+	terminalExpress() = delete;
+	terminalExpress(string str) {
+		key = str;
+	}
+	virtual int interpret(Context context) {
+		context.lookUp(this);
+	}
+private:
+	string key;
+};
+//非终端命令
+class AndTerminalExpress :public express {
+public:
+	AndTerminalExpress() = delete;
+	AndTerminalExpress(express* l, express* r) {
+		left = l;
+		right = r;
+	}
+	virtual int interpret(Context context) {
+		return left->interpret(context) + right->interpret(context);
+	}
+private:
+	express* left;
+	express* right;
+};
+//****************************************行为型模式******************************************************************************//
+//5555555555555555555555555555555**********命令模式**********5555555555555555555555555555//
+//666666666666666666666666666666**********观察者模式**********666666666666666666666666666666666//
+//******************************************************************************//
+//此处运用了命令模式和观察者结合
+
+//同一个command能注册多个receive，同一个receive也可以注册到多个command中
+class command {
+public:
+	virtual void call() {
+		for (auto m : m_listRecv) {
+			m->run();
+		}
+	}
+	virtual ~command() {
+		for (auto c : m_listRecv) {
+			if (c)
+				c->noReCurRemoveCommand(this);
+		}
+	}
+private:
+	/////////////////此处仅允许receive使用////////////////////////
+
+	virtual void add(receive* recv) final{
+		if (recv) 
+		{
+			try {
+				for (auto &n : m_listRecv) {
+					if (n == recv)
+						throw 0;
+				}
+			}
+			catch (int n) {
+				printf("命令中已有该接受者:%s,%s", __FILE__, __LINE__);
+				return;
+			}
+			m_listRecv.push_back(recv);
+		}
+
+	}
+	virtual void remove(receive* recv) final{
+		if (recv) {
+			auto it = m_listRecv.begin();
+			for (; it != m_listRecv.end(); it++) {
+				if (*it == recv)
+				{
+					m_listRecv.erase(it);
+					return;
+				}
+			}
+		}
+	}
+	friend class receive;
+protected:
+	std::list<receive*> m_listRecv;
+};
+class receive {
+private:
+	std::list<command*> m_listComm;
+public: 
+	virtual ~receive() {
+		for (auto c: m_listComm) {
+			if(c)
+				c->remove(this);
+			}
+	}
+	virtual void setCommand(command* c) final {
+		if (c) {
+			try {
+				for (auto &n : m_listComm) {
+					if (n == c)
+						throw 0;
+				}
+			}
+			catch (int n) {
+				printf("命令中已有该接受者:%s,%s", __FILE__, __LINE__);
+				return;
+			}
+			m_listComm.push_back(c);
+			c->add(this);
+		}
+	}
+	virtual void removeCommand(command* c) final {
+		if (c)
+		{
+			auto it = m_listComm.begin();
+			for (; it != m_listComm.end(); it++) {
+				if (*it == c)
+				{
+					m_listComm.erase(it);
+					c->remove(this);
+					return;
+				}
+			}
+		}
+	}
+	virtual void run() = 0;
+protected:
+	virtual void noReCurRemoveCommand(command* c) final {
+		if (c)
+		{
+			auto it = m_listComm.begin();
+			for (; it != m_listComm.end(); it++) {
+				if (*it == c)
+				{
+					m_listComm.erase(it);
+					return;
+				}
+			}
+		}
+	}
+	friend class command;
+};
+class receive1 {
+public:
+	virtual void run() {}
+};
+class invoke {
+public:
+	invoke() = delete;
+	invoke(command* c) {m_c = c;}
+	void setCommand(command* c) { m_c = c; }
+	void call() { 
+		if(m_c)
+			m_c->call();
+	}
+private:
+	command* m_c;
+};
+
 //*************************行为型模式******************************************************************************//
-//55555555555555555555555555555555555**********命令模式**********55555555555555555555555555555555555555//
-//******************************************************************************//y
-对
+//777777777777777777777777777777777**********中介者模式**********77777777777777777777777777777777777777777777777//
+//******************************************************************************//
+//与观察者类似，不过消息的发送者为接收者中的一个
+//代码参考观察者
+
+
+//*************************行为型模式******************************************************************************//
+//8888888888888888888888888888888888888888**********访问者模式**********888888888888888888888888888888888888888//
+//******************************************************************************//
+//与观察者类似，不过消息的发送者为接收者中的一个
+class medium {
+public:
+	void accept(BaseVisit *v) {
+		if (v) {
+			for (auto e : m_listElement) {
+				if (e)
+					e->accept(v);
+			}
+		}
+	}
+	void add(BaseElement* e) {
+		m_listElement.push_back(e);
+	}
+	void remove(BaseElement* e) {
+		m_listElement.remove(e);
+	}
+private:
+	std::list<BaseElement*> m_listElement;
+};
+class BaseElement {
+public:
+	virtual void accept(BaseVisit* v) = 0;
+};
+class element1 :public BaseElement {
+public:
+	void accept(BaseVisit* v) {
+		if (v)
+			v->visit(this);
+	}
+};
+class element2 :public BaseElement {
+public:
+	void accept(BaseVisit* v) {
+		if (v)
+			v->visit(this);
+	}
+};
+class BaseVisit {
+public:
+	virtual void visit(BaseElement* e) = 0;
+};
+class visit1 {
+public:
+	virtual void visit(BaseElement* e) {}
+};
+class visit2 {
+public:
+	virtual void visit(BaseElement* e) {}
+};
+//*************************行为型模式******************************************************************************//
+//9999999999999999999999999999999999**********备忘录模式**********999999999999999999999999999999999999999//
+//******************************************************************************//
+//运用克隆相结合
+class memoryManage {
+public:
+	memoryManage() = delete;
+	memoryManage(BaseMemory* pMomery):m_pMomery(pMomery){
+		try {
+			if (pMomery == nullptr)
+				throw 0;
+		}
+		catch (int) {
+			printf("传入了空值：%s-%s", __FILE__, __LINE__);
+		}
+	}
+	void saveMemory() {
+		if(m_pMomery)
+			m_store.push(m_pMomery);
+	}
+	void movePre() {
+		BaseMemory* p = m_store.pop();
+		if (p) {
+			delete m_pMomery;
+			m_pMomery = p;
+		}			
+	}
+private:
+	BaseMemory* m_pMomery;
+	store m_store;
+};
+
+class BaseMemory {
+public:
+	virtual BaseMemory* clone() = 0;
+};
+class memory:public BaseMemory {
+public:
+	virtual BaseMemory* clone() { 
+		return new memory(*this); //默认浅拷贝，深拷贝需要自己实现，具体参考上面的原型模式
+	}
+private:
+	int a;
+};
+
+class store {
+public:
+	void push(BaseMemory* o) {
+		if(o)
+			m_stackMemory.push(o);
+	}
+	BaseMemory* pop() {
+		BaseMemory* res = m_stackMemory.top();
+		m_stackMemory.pop();
+		return res;
+	}
+private:
+	stack<BaseMemory*> m_stackMemory;
+};
+//*************************行为型模式******************************************************************************//
+//10 10 10 10 10 10 10**********责任链模式**********10 10 10 10 10 10 10// 
+//******************************************************************************//
+class handle {
+public:
+	void setNext(handle* p) { next = p; }
+	handle* getNext()const { return next; }
+	virtual void handleRequest(int n) = 0;
+protected:
+	int m_v;
+private:
+	handle* next;
+};
+class handle1 :public handle {
+public:
+	virtual void handleRequest(int n){
+		if (n == m_v) {
+			//Todo
+		}
+		else {
+			if (getNext()) {
+				getNext()->handleRequest(n);
+				
+			}
+			else {
+				printf("no one handle the request.");
+			}
+		}
+
+	}
+};
+//*************************行为型模式******************************************************************************//
+//11 11 11 11 11 11 11**********迭代器模式**********11 11 11 11 11 11 11//
+//******************************************************************************//
+class Object{};
+
+class agregate {
+public:
+	virtual void add(Object*) = 0;
+	virtual void remove(Object*) = 0;
+	virtual Iterator* getIterator() = 0;
+};
+class agregate1 {
+public:
+	virtual void add(Object* o) {
+		if(o)
+			m_listData->push_back(o);
+	}
+	virtual void remove(Object* o) {
+		if (o)
+			m_listData->remove(o);
+	}
+	virtual Iterator* getIterator() {
+		return dynamic_cast<Iterator *>(new Iterator1(m_listData));
+	}
+private:
+	list<Object*> *m_listData = new list<Object*>;
+};
+
+class Iterator {
+public:
+	virtual bool hasNext() = 0;
+	virtual void first() = 0;
+	virtual agregate* next() = 0;
+};
+class Iterator1 {
+public:
+	Iterator1(list<Object*> *p){
+		m_listData = p;
+		i = -1;
+	}
+	virtual bool hasNext() {
+		if (i < m_listData->size() - 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	virtual Object* first() {
+		return m_listData->front();
+	}
+	virtual Object* next() {
+		Object* o = nullptr;
+		if (hasNext()) {
+			int k = ++i;
+			auto it = m_listData->begin();
+			while (k--) {
+				++it;
+			}
+			o = *it;
+		}
+		return o;
+	}
+private:
+	list<Object*> *m_listData = nullptr;
+	int i;
+};
